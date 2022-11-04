ff92acf63a2eeafea76565b4fdee8a375a4c5c1d
Muhammad Sahlan Shadiq <103637462+alansahlan65@users.noreply.github.com>
2022-11-04 18:45:04 +0700

    Update Demo.cpp

--- Demo.cpp
+++ Demo.cpp
@@ -1,5 +1,6 @@
 #include "Demo.h"
 
+///PROJECT AKHIR GRAFKOM
 
 
 Demo::Demo() {
@@ -17,9 +18,15 @@ void Demo::Init() {
 	// ------------------------------------
 	shaderProgram = BuildShader("vertexShader.vert", "fragmentShader.frag", nullptr);
 
+	BuildColoredRoom();
+
 	BuildColoredCube();
 
+	BuildColoredKursi();
+
 	BuildColoredPlane();
+
+	InitCamera();
 }
 
 void Demo::DeInit() {
@@ -35,14 +42,78 @@ void Demo::DeInit() {
 
 // process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
 // ---------------------------------------------------------------------------------------------------------
-void Demo::ProcessInput(GLFWwindow *window) {
+void Demo::ProcessInput(GLFWwindow* window) {
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
 		glfwSetWindowShouldClose(window, true);
 	}
+
+	// zoom camera
+	// -----------
+	if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_RIGHT) == GLFW_PRESS) {
+		if (fovy < 90) {
+			fovy += 0.0001f;
+		}
+	}
+
+	if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS) {
+		if (fovy > 0) {
+			fovy -= 0.0001f;
+		}
+	}
+
+	// update camera movement 
+	// -------------
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
+		MoveCamera(CAMERA_SPEED);
+	}
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
+		MoveCamera(-CAMERA_SPEED);
+	}
+
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
+		StrafeCamera(-CAMERA_SPEED);
+	}
+
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
+		StrafeCamera(CAMERA_SPEED);
+	}
+
+	// update camera rotation
+	// ----------------------
+	double mouseX, mouseY;
+	double midX = screenWidth / 2;
+	double midY = screenHeight / 2;
+	float angleY = 0.0f;
+	float angleZ = 0.0f;
+
+	// Get mouse position
+	glfwGetCursorPos(window, &mouseX, &mouseY);
+	if ((mouseX == midX) && (mouseY == midY)) {
+		return;
+	}
+
+	// Set mouse position
+	glfwSetCursorPos(window, midX, midY);
+
+	// Get the direction from the mouse cursor, set a resonable maneuvering speed
+	angleY = (float)((midX - mouseX)) / 1000;
+	angleZ = (float)((midY - mouseY)) / 1000;
+
+	// The higher the value is the faster the camera looks around.
+	viewCamY += angleZ * 2;
+
+	// limit the rotation around the x-axis
+	if ((viewCamY - posCamY) > 8) {
+		viewCamY = posCamY + 8;
+	}
+	if ((viewCamY - posCamY) < -8) {
+		viewCamY = posCamY - 8;
+	}
+	RotateCamera(-angleY);
 }
 
 void Demo::Update(double deltaTime) {
-	angle += (float) ((deltaTime * 1.5f) / 1000);
+
 }
 
 void Demo::Render() {
@@ -61,17 +132,142 @@ void Demo::Render() {
 	glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
 
 	// LookAt camera (position, target/direction, up)
-	glm::mat4 view = glm::lookAt(glm::vec3(0, 3, 5), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
+	glm::mat4 view = glm::lookAt(glm::vec3(posCamX, posCamY, posCamZ), glm::vec3(viewCamX, viewCamY, viewCamZ), glm::vec3(upCamX, upCamY, upCamZ));
 	GLint viewLoc = glGetUniformLocation(this->shaderProgram, "view");
 	glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
 
+	DrawColoredRoom();
+
 	DrawColoredCube();
+
+	DrawColoredKursi();
 
 	DrawColoredPlane();
 
 	glDisable(GL_DEPTH_TEST);
 }
 
+//-----------BUILD RUANGAN
+void Demo::BuildColoredRoom() {
+	// load image into texture memory
+	// ------------------------------
+	// Load and create a texture 
+	glGenTextures(1, &texture0);
+	glBindTexture(GL_TEXTURE_2D, texture0);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	int width, height;
+	unsigned char* image = SOIL_load_image("wall.png", &width, &height, 0, SOIL_LOAD_RGBA);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
+	SOIL_free_image_data(image);
+	glBindTexture(GL_TEXTURE_2D, 0);
+
+	// set up vertex data (and buffer(s)) and configure vertex attributes
+	// ------------------------------------------------------------------
+	float vertices[] = {
+		// format position, tex coords
+		// front
+		-0.5, -0.5, 0.5, 0, 0,  // 0
+		0.5, -0.5, 0.5, 1, 0,   // 1
+		0.5,  0.5, 0.5, 1, 1,   // 2
+		-0.5,  0.5, 0.5, 0, 1,  // 3
+
+		// right
+		0.5,  0.5,  0.5, 0, 0,  // 4
+		0.5,  0.5, -0.5, 1, 0,  // 5
+		0.5, -0.5, -0.5, 1, 1,  // 6
+		0.5, -0.5,  0.5, 0, 1,  // 7
+
+		// back
+		-0.5, -0.5, -0.5, 0, 0, // 8 
+		0.5,  -0.5, -0.5, 1, 0, // 9
+		0.5,   0.5, -0.5, 1, 1, // 10
+		-0.5,  0.5, -0.5, 0, 1, // 11
+
+		// left
+		-0.5, -0.5, -0.5, 0, 0, // 12
+		-0.5, -0.5,  0.5, 1, 0, // 13
+		-0.5,  0.5,  0.5, 1, 1, // 14
+		-0.5,  0.5, -0.5, 0, 1, // 15
+
+		// upper
+		0.5, 0.5,  0.5, 0, 0,   // 16
+		-0.5, 0.5,  0.5, 1, 0,  // 17
+		-0.5, 0.5, -0.5, 1, 1,  // 18
+		0.5, 0.5, -0.5, 0, 1,   // 19
+
+		// bottom
+		-0.5, -0.5, -0.5, 0, 0, // 20
+		0.5, -0.5, -0.5, 1, 0,  // 21
+		0.5, -0.5,  0.5, 1, 1,  // 22
+		-0.5, -0.5,  0.5, 0, 1, // 23
+	};
+
+	unsigned int indices[] = {
+		0,  1,  2,  0,  2,  3,   // front
+		4,  5,  6,  4,  6,  7,   // right
+		8,  9,  10, 8,  10, 11,  // back
+		12, 14, 13, 12, 15, 14,  // left
+		16, 18, 17, 16, 19, 18,  // upper
+		20, 22, 21, 20, 23, 22   // bottom
+	};
+
+	glGenVertexArrays(1, &VAO0);
+	glGenBuffers(1, &VBO0);
+	glGenBuffers(1, &EBO0);
+	// bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+	glBindVertexArray(VAO0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO0);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO0);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	// define position pointer layout 0
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)(0 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(0);
+
+	// define texcoord pointer layout 1
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(1);
+
+	// note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	// You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
+	// VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+	glBindVertexArray(0);
+
+	// remember: do NOT unbind the EBO while a VAO is active as the bound element buffer object IS stored in the VAO; keep the EBO bound.
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+}
+
+void Demo::DrawColoredRoom()
+{
+	glUseProgram(shaderProgram);
+
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, texture0);
+	glUniform1i(glGetUniformLocation(this->shaderProgram, "ourTexture"), 0);
+
+	glBindVertexArray(VAO0); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+	
+	glm::mat4 model;
+	model = glm::translate(model, glm::vec3(0, 0, -5));
+	model = glm::scale(model, glm::vec3(20, 30, 20));
+
+	GLint modelLoc = glGetUniformLocation(this->shaderProgram, "model");
+	glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+	
+	glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+
+	glBindVertexArray(0);
+
+}
+
+//-----------BUILD MEJA
 void Demo::BuildColoredCube() {
 	// load image into texture memory
 	// ------------------------------
@@ -243,6 +439,177 @@ void Demo::DrawColoredCube()
 
 }
 
+//-----------BUILD KURSI
+void Demo::BuildColoredKursi() {
+	// load image into texture memory
+	// ------------------------------
+	// Load and create a texture 
+	glGenTextures(1, &texture3);
+	glBindTexture(GL_TEXTURE_2D, texture3);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	int width, height;
+	unsigned char* image = SOIL_load_image("kursi.png", &width, &height, 0, SOIL_LOAD_RGBA);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
+	SOIL_free_image_data(image);
+	glBindTexture(GL_TEXTURE_2D, 0);
+
+	// set up vertex data (and buffer(s)) and configure vertex attributes
+	// ------------------------------------------------------------------
+	float vertices[] = {
+		// format position, tex coords
+		// front
+		-0.5, -0.5, 0.5, 0, 0,  // 0
+		0.5, -0.5, 0.5, 1, 0,   // 1
+		0.5,  0.5, 0.5, 1, 1,   // 2
+		-0.5,  0.5, 0.5, 0, 1,  // 3
+
+		// right
+		0.5,  0.5,  0.5, 0, 0,  // 4
+		0.5,  0.5, -0.5, 1, 0,  // 5
+		0.5, -0.5, -0.5, 1, 1,  // 6
+		0.5, -0.5,  0.5, 0, 1,  // 7
+
+		// back
+		-0.5, -0.5, -0.5, 0, 0, // 8 
+		0.5,  -0.5, -0.5, 1, 0, // 9
+		0.5,   0.5, -0.5, 1, 1, // 10
+		-0.5,  0.5, -0.5, 0, 1, // 11
+
+		// left
+		-0.5, -0.5, -0.5, 0, 0, // 12
+		-0.5, -0.5,  0.5, 1, 0, // 13
+		-0.5,  0.5,  0.5, 1, 1, // 14
+		-0.5,  0.5, -0.5, 0, 1, // 15
+
+		// upper
+		0.5, 0.5,  0.5, 0, 0,   // 16
+		-0.5, 0.5,  0.5, 1, 0,  // 17
+		-0.5, 0.5, -0.5, 1, 1,  // 18
+		0.5, 0.5, -0.5, 0, 1,   // 19
+
+		// bottom
+		-0.5, -0.5, -0.5, 0, 0, // 20
+		0.5, -0.5, -0.5, 1, 0,  // 21
+		0.5, -0.5,  0.5, 1, 1,  // 22
+		-0.5, -0.5,  0.5, 0, 1, // 23
+	};
+
+	unsigned int indices[] = {
+		0,  1,  2,  0,  2,  3,   // front
+		4,  5,  6,  4,  6,  7,   // right
+		8,  9,  10, 8,  10, 11,  // back
+		12, 14, 13, 12, 15, 14,  // left
+		16, 18, 17, 16, 19, 18,  // upper
+		20, 22, 21, 20, 23, 22   // bottom
+	};
+
+	glGenVertexArrays(1, &VAO3);
+	glGenBuffers(1, &VBO3);
+	glGenBuffers(1, &EBO3);
+	// bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+	glBindVertexArray(VAO3);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO3);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO3);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	// define position pointer layout 0
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)(0 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(0);
+
+	// define texcoord pointer layout 1
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(1);
+
+	// note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	// You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
+	// VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+	glBindVertexArray(0);
+
+	// remember: do NOT unbind the EBO while a VAO is active as the bound element buffer object IS stored in the VAO; keep the EBO bound.
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+}
+
+void Demo::DrawColoredKursi()
+{
+	glUseProgram(shaderProgram);
+
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, texture3);
+	glUniform1i(glGetUniformLocation(this->shaderProgram, "ourTexture"), 0);
+
+	glBindVertexArray(VAO3); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+
+	//Atas Kursi
+	glm::mat4 model;
+	model = glm::translate(model, glm::vec3(0, 0.25, -1.2));
+	model = glm::scale(model, glm::vec3(1, 0.1, 1));
+
+	GLint modelLoc = glGetUniformLocation(this->shaderProgram, "model");
+	glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+
+	glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+
+	//Kaki Belakang kanan
+	glm::mat4 model2;
+	model2 = glm::translate(model2, glm::vec3(0.45, -0.1, -1.65));
+	model2 = glm::scale(model2, glm::vec3(0.1, 2.3, 0.1));
+
+	glGetUniformLocation(this->shaderProgram, "model");
+	glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model2));
+
+	glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+
+	//Kaki Depan kanan
+	glm::mat4 model3;
+	model3 = glm::translate(model3, glm::vec3(-0.45, -0.1, -1.65));
+	model3 = glm::scale(model3, glm::vec3(0.1, 2.3, 0.1));
+
+	glGetUniformLocation(this->shaderProgram, "model");
+	glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model3));
+
+	glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+
+	//Kaki Belakang kiri
+	glm::mat4 model4;
+	model4 = glm::translate(model4, glm::vec3(0.45, -0.1, -0.75));
+	model4 = glm::scale(model4, glm::vec3(0.1, 0.8, 0.1));
+
+	glGetUniformLocation(this->shaderProgram, "model");
+	glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model4));
+
+	glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+
+	//Kaki Depan kiri
+	glm::mat4 model5;
+	model5 = glm::translate(model5, glm::vec3(-0.45, -0.1, -0.75));
+	model5 = glm::scale(model5, glm::vec3(0.1, 0.8, 0.1));
+
+	glGetUniformLocation(this->shaderProgram, "model");
+	glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model5));
+
+	glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+
+	//Senderan
+	glm::mat4 model6;
+	model6 = glm::translate(model6, glm::vec3(0, 0.9, -1.65));
+	model6 = glm::scale(model6, glm::vec3(0.8, 0.3, 0.1));
+
+	glGetUniformLocation(this->shaderProgram, "model");
+	glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model6)); 
+
+	glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+
+	glBindVertexArray(0);
+
+}
+
 void Demo::BuildColoredPlane()
 {
 	// Load and create a texture 
@@ -268,8 +635,6 @@ void Demo::BuildColoredPlane()
 		 50.0, -0.5, -50.0, 50,  0,
 		 50.0, -0.5,  50.0, 50, 50,
 		-50.0, -0.5,  50.0,  0, 50,
-
-
 	};
 
 	GLuint indices[] = { 0,  2,  1,  0,  3,  2 };
@@ -296,8 +661,6 @@ void Demo::BuildColoredPlane()
 	glBindVertexArray(0); // Unbind VAO
 }
 
-
-
 void Demo::DrawColoredPlane()
 {
 	glUseProgram(shaderProgram);
@@ -316,6 +679,57 @@ void Demo::DrawColoredPlane()
 
 	glBindTexture(GL_TEXTURE_2D, 0);
 	glBindVertexArray(0);
+}
+
+//FREE CAMERA
+void Demo::InitCamera()
+{
+	posCamX = 0.0f;
+	posCamY = 1.0f;
+	posCamZ = 8.0f;
+	viewCamX = 0.0f;
+	viewCamY = 1.0f;
+	viewCamZ = 0.0f;
+	upCamX = 0.0f;
+	upCamY = 1.0f;
+	upCamZ = 0.0f;
+	CAMERA_SPEED = 0.001f;
+	fovy = 45.0f;
+	glfwSetInputMode(this->window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+}
+
+
+void Demo::MoveCamera(float speed)
+{
+	float x = viewCamX - posCamX;
+	float z = viewCamZ - posCamZ;
+	// forward positive cameraspeed and backward negative -cameraspeed.
+	posCamX = posCamX + x * speed;
+	posCamZ = posCamZ + z * speed;
+	viewCamX = viewCamX + x * speed;
+	viewCamZ = viewCamZ + z * speed;
+}
+
+void Demo::StrafeCamera(float speed)
+{
+	float x = viewCamX - posCamX;
+	float z = viewCamZ - posCamZ;
+	float orthoX = -z;
+	float orthoZ = x;
+
+	// left positive cameraspeed and right negative -cameraspeed.
+	posCamX = posCamX + orthoX * speed;
+	posCamZ = posCamZ + orthoZ * speed;
+	viewCamX = viewCamX + orthoX * speed;
+	viewCamZ = viewCamZ + orthoZ * speed;
+}
+
+void Demo::RotateCamera(float speed)
+{
+	float x = viewCamX - posCamX;
+	float z = viewCamZ - posCamZ;
+	viewCamZ = (float)(posCamZ + glm::sin(speed) * x + glm::cos(speed) * z);
+	viewCamX = (float)(posCamX + glm::cos(speed) * x - glm::sin(speed) * z);
 }
 
 int main(int argc, char** argv) {
