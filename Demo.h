f31c6726e98cc22839eb40913d118ad692990f9a
Muhammad Sahlan Shadiq <103637462+alansahlan65@users.noreply.github.com>
2022-11-11 20:55:41 +0700

    Update Demo.h

--- Demo.h
+++ Demo.h
@@ -17,24 +17,44 @@ private:
 	GLuint shaderProgram, VBO, VAO, EBO, texture; //Meja
 	GLuint VBO2, VAO2, EBO2, texture2; //Lantai
 	GLuint VBO3, VAO3, EBO3, texture3; //Kursi
+	GLuint VBO4, VAO4, EBO4, texture4; //Kasur
+	GLuint VBO5, VAO5, EBO5, texture5; //Bantal
+	GLuint VBO6, VAO6, EBO6, texture6; //PenyanggaKasur
+	GLuint VBO7, VAO7, EBO7, texture7; //Pintu
 	float viewCamX, viewCamY, viewCamZ, upCamX, upCamY, upCamZ, posCamX, posCamY, posCamZ, CAMERA_SPEED, fovy;
 	virtual void Init();
 	virtual void DeInit();
 	virtual void Update(double deltaTime);
 	virtual void Render();
-	virtual void ProcessInput(GLFWwindow *window);
+	virtual void ProcessInput(GLFWwindow* window);
 	//Ruangan
 	void BuildColoredRoom();
 	void DrawColoredRoom();
+	//Jendela
+	void BuildColoredJendela();
+	void DrawColoredJendela();
 	//Meja
 	void BuildColoredCube();
 	void DrawColoredCube();
 	//Kursi
 	void BuildColoredKursi();
 	void DrawColoredKursi();
+	//Kasur
+	void BuildColoredKasur();
+	void DrawColoredKasur();
+	//Bantal
+	void BuildColoredBantal();
+	void DrawColoredBantal();
+	//PenyanggaKasur
+	void BuildColoredPenyanggakasur();
+	void DrawColoredPenyanggakasur();
+	//Pintu
+	void BuildColoredPintu();
+	void DrawColoredPintu();
 	//Lantai
 	void BuildColoredPlane();
 	void DrawColoredPlane();
+	//Camera
 	void MoveCamera(float speed);
 	void StrafeCamera(float speed);
 	void RotateCamera(float speed);
