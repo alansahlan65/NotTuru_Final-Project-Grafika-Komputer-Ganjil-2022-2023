78b1d7407ab12756d269247788910efaf5e607cc
Muhammad Sahlan Shadiq <103637462+alansahlan65@users.noreply.github.com>
2022-11-18 21:27:19 +0700

    Update Demo.h

--- Demo.h
+++ Demo.h
@@ -21,6 +21,15 @@ private:
 	GLuint VBO5, VAO5, EBO5, texture5; //Bantal
 	GLuint VBO6, VAO6, EBO6, texture6; //PenyanggaKasur
 	GLuint VBO7, VAO7, EBO7, texture7; //Pintu
+	GLuint VBO8, VAO8, EBO8, texture8; //Komputer
+	GLuint VBO9, VAO9, EBO9, texture9; //Layar Komputer
+	GLuint VBO10, VAO10, EBO10, texture10; //Keyboard
+	GLuint VBO11, VAO11, EBO11, texture11; //Body Keyboard
+	GLuint VBO12, VAO12, EBO12, texture12; //Mouse
+	GLuint VBO13, VAO13, EBO13, texture13; //CPU
+	GLuint VBO14, VAO14, EBO14, texture14; //Depan CPU
+	GLuint VBO15, VAO15, EBO15, texture15; //Lemari
+	GLuint VBO16, VAO16, EBO16, texture16; //Depan Lemari
 	float viewCamX, viewCamY, viewCamZ, upCamX, upCamY, upCamZ, posCamX, posCamY, posCamZ, CAMERA_SPEED, fovy;
 	virtual void Init();
 	virtual void DeInit();
@@ -30,6 +39,9 @@ private:
 	//Ruangan
 	void BuildColoredRoom();
 	void DrawColoredRoom();
+	//Jendela
+	void BuildColoredJendela();
+	void DrawColoredJendela();
 	//Meja
 	void BuildColoredCube();
 	void DrawColoredCube();
@@ -48,6 +60,33 @@ private:
 	//Pintu
 	void BuildColoredPintu();
 	void DrawColoredPintu();
+	//Komputer
+	void BuildColoredKomputer();
+	void DrawColoredKomputer();
+	//Layar Komputer
+	void BuildColoredLayar();
+	void DrawColoredLayar();
+	//Keyboard
+	void BuildColoredKeyboard();
+	void DrawColoredKeyboard();
+	//Body Keyboard
+	void BuildColoredBodykeyboard();
+	void DrawColoredBodykeyboard();
+	//Mouse
+	void BuildColoredMouse();
+	void DrawColoredMouse();
+	//CPU
+	void BuildColoredCPU();
+	void DrawColoredCPU();
+	//Depan CPU
+	void BuildColoredDepanCPU();
+	void DrawColoredDepanCPU();
+	//Lemari
+	void BuildColoredLemari();
+	void DrawColoredLemari();
+	//Depan Lemari
+	void BuildColoredDepanLemari();
+	void DrawColoredDepanLemari();
 	//Lantai
 	void BuildColoredPlane();
 	void DrawColoredPlane();
